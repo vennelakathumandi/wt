4434c5f402ac2440cb43dc91947018a3574da5e4
vennelakathumandi <124117551+vennelakathumandi@users.noreply.github.com>
2023-02-01 12:07:57 +0530

    Add files via upload

---
+++ FatArrowDemo.js
@@ -0,0 +1,31 @@
+/*let x=30;
+console.log(x);
+for(let i=0;i<5;i++)
+{
+    console.log(i);
+}
+if(true)
+{
+    let x=50;
+    console.log("inside value"+x);
+}
+console.log("outside value"+x);
+
+
+let i=2;
+let j=4;
+console.log(i);*/
+
+var hello=()=>
+{
+return "hello";
+}
+console.log(hello());
+var sum=(a,b)=>
+{a+b;
+
+console.log(sum(9,8));
+}
+
+
+
